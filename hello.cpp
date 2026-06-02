30076b5de491d510c1c7a8ded27430a2145dc214
Dhruv <dhruvsen681@gmail.com>
2026-06-02 15:08:46 +0530

    feat: initialize C++ project with basic arithmetic calculator and VS Code build configuration

---
+++ hello.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int a, b;
+  cout << "Enter the value of a = ";
+  cin >> a;
+  cout << "Enter the value of b = ";
+  cin >> b;
+  char c;
+  cout << "enter the operation : ";
+  cin >> c;
+  char d, e, f, g;
+  d = '+';
+  e = '-';
+  f = '*';
+  g = '/';
+  if (c == d) {
+    cout << "ADDITION = " << a + b;
+  } else if (c == e) {
+    cout << "SUBSTRACTION = " << a - b;
+  } else if (c == f) {
+    cout << "MULTIPLICATION = " << a * b;
+  } else if (c == g) {
+    cout << "DIVISION = " << a / b;
+  } else {
+    cout << "INVALID OPERATION";
+  }
+}
