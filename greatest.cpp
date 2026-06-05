a6d071b5864b0ba2177cc2ea100caa33950e41eb
Dhruv <dhruvsen681@gmail.com>
2026-06-05 20:41:43 +0530

    feat: add greatest and hello programs, configure VS Code environment, and disable table.cpp

---
+++ greatest.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int main() {
+  float a, b, c;
+  cout << "Enter the value of a = ";
+  cin >> a;
+  cout << "Enter the value of b = ";
+  cin >> b;
+  cout << "Enter the value of c = ";
+  cin >> c;
+
+  if (a == b && b == c) {
+    cout << "a and b and c are all equal";
+  } else if (a == b && a > c) {
+    cout << "a and b are the greatest";
+  } else if (a == c && a > b) {
+    cout << "a and c are the greatest";
+  } else if (b == c && b > a) {
+    cout << "b and c are the greatest";
+  } else if (a > b && a > c) {
+    cout << "A is the greatest of all";
+  } else if (b > a && b > c) {
+    cout << "B is the greatest of all";
+  } else if (c > a && c > b) {
+    cout << "C is the greatest of all";
+  }
+
+  return 0;
+}
--- table.cpp
+++ table.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+/*#include<iostream>
 using namespace std;
 int main (){
     int n ;
@@ -10,4 +10,4 @@ int main (){
     }
     return 0;
 
-}
+}*/
