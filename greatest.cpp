944606546b973aa8cb31356a227f25581b8ae581
Dhruv <dhruvsen681@gmail.com>
2026-06-05 20:58:33 +0530

    feat: implement nested conditional logic for finding the greatest number in a new file and comment out the original implementation

--- greatest.cpp
+++ greatest.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+/*#include <iostream>
 using namespace std;
 int main() {
   float a, b, c;
@@ -25,4 +25,4 @@ int main() {
     cout << "C is the greatest of all";
   }
   return 0;
-}
+}*/
---
+++ nested-greatest.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main() {
+  float a, b, c;
+  cout << "Enter the value of a = ";
+  cin >> a;
+  cout << "Enter the value of b = ";
+  cin >> b;
+  cout << "Enter the value of c = ";
+  cin >> c;
+
+  if (a > c) {
+    if (a > b) {
+      cout << " a is the greatest of all";
+    } else {
+      cout << " b is the greatest of all";
+    }
+  } else {
+    cout << " c is the greatest of all";
+  }
+  return 0;
+}
