b6d929020b09d5515cab3032e5c59108c68e46d0
Dhruv <dhruvsen681@gmail.com>
2026-06-11 10:05:26 +0530

    feat: add multiplication table generator to loop.cpp

--- loop.cpp
+++ loop.cpp
@@ -21,3 +21,14 @@ int main() {
   return 0;
 }
 */
+/*#include <iostream>
+using namespace std;
+int main() {
+  int n;
+  cout << "enter the number: ";
+  cin >> n;//2*2=4
+  for (int i = 1; i <= 10; i++) {
+    cout<<n<<"*"<<i<<"="<<n*i<<endl;
+  }
+  return 0;
+}*/
