58a3c0928796f7aadc70b26f84c0b8161cc6383b
Dhruv <dhruvsen681@gmail.com>
2026-06-11 10:37:43 +0530

    Please provide the specific code changes or the git diff so I can generate an accurate summary for you.

--- loop.cpp
+++ loop.cpp
@@ -66,13 +66,30 @@ int main() {
   }
   return 0;
 }*/
+/*#include <iostream>
+using namespace std;
+int main() {
+  int a,d,n;
+  cout << "enter the first term of AP : ";
+  cin >> a;
+  cout << "enter the common difference of AP : ";
+  cin >> d;
+  cout << "enter the number of terms of AP : ";
+  cin >> n;
+  int term = a;
+  for (int i = 1; i <= 2 * n - 1; i = i + 2) {
+    cout << "ap -" << term << ",";
+    term =term + d;
+  }
+  return 0;
+}*/
 #include <iostream>
 using namespace std;
 int main() {
   int n;
   cout << "enter the number: ";
   cin >> n;
-  for (int i = 1; i <= 2 * n - 1; i = i + 2) {
+  for (int i = 1; i <= n; i) {
     cout << "ap -" << i << ",";
   }
   return 0;
