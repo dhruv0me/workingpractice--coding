681774da2509cbf9fcf793c3b60cbc62dae78f0f
Dhruv <dhruvsen681@gmail.com>
2026-06-12 10:53:17 +0530

    feat: implement prime number detection logic in break.cpp

--- break.cpp
+++ break.cpp
@@ -10,16 +10,30 @@ int main() {
   return 0;
 }
   */
-  #include <iostream>
+/*#include <iostream>
+using namespace std;
+int main() {
+int n;
+cout << "enter the number: ";
+cin >> n;
+for (int i = n / 2; i >= 1; i--) {
+  if (n % i == 0)
+  {  cout<<i<<endl;
+      break;
+}
+}
+return 0;
+}*/
+/*#include <iostream>
 using namespace std;
 int main() {
   int n;
   cout << "enter the number: ";
   cin >> n;
-  int f = 1;
-  for (int i = 1; i <= n / 2; i++) {
-    if (n % i == 0) f = i;
-  }
-  cout << f;
-  return 0;
-}
+  bool flag = true;
+  for (int i =2 ;i<n/2;i++)
+  { if(n%i==0){flag=false;break;}}
+  if(n==1){cout<<"neither prime nor composite";}
+  else if(flag==true){cout<<"prime";}else{cout<<"Composite";};
+   return 0;
+}*/
